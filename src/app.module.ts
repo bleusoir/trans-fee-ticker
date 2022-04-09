e4a047ecf9270384c2f2b087230704854cb875dc
JY, Park <bleusoire@gmail.com>
2022-04-09 23:58:39 +0900

    added typeorm.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CurrencyModule } from './currency/currency.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { env } from 'process';
+
+const DB_TYPE = env.DB_TYPE,
+  DB_HOST = env.DB_HOST,
+  DB_PORT = env.DB_PORT,
+  DB_USER = env.DB_USER,
+  DB_PASS = env.DB_PASS,
+  DB_NAME = env.DB_NAME;
 
 @Module({
-  imports: [ConfigModule.forRoot(), CurrencyModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CurrencyModule,
+    TypeOrmModule.forRoot({
+      type: DB_TYPE,
+      host: DB_HOST,
+      port: DB_PORT,
+      username: DB_USER,
+      password: DB_PASS,
+      database: DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(
+    private readonly config: ConfigService
+  ) {
+  }
+}
